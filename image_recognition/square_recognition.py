076e51fc6ff1aaf94f4b32ee8de934f86b33ec08
Kevin <Kevin-Roberts@users.noreply.github.com>
2014-02-04 10:43:47 -0600

    Don't need res, more comments
    Removed step to return color to the mask because findContours used the binary (black/white only) mask image anyway.

--- image_recognition/square_recognition.py
+++ image_recognition/square_recognition.py
@@ -7,13 +7,11 @@ import cv2
 im = cv2.imread('blocks.jpg')
 # Convert to HSV, easier to isolate colors
 hsv = cv2.cvtColor(im,cv2.COLOR_BGR2HSV)
-# Range of color
+# Range of color hard coded for the dark blue block
 lower_blue = np.array([105,50,50])
 upper_blue = np.array([115,255,255])
 # Extract new range of color
 mask = cv2.inRange(hsv, lower_blue, upper_blue)
-# AND with original image
-res = cv2.bitwise_and(im,im, mask= mask)
 # Find color boundaries
 contours, hierarchy = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
@@ -23,8 +21,8 @@ for contour in contours:
 	area = cv2.contourArea(contour)
 	if area > 1000:
 		blocks.append(contour)
-# Draw the contours
+# Draw the contours just so that we can see that it worked.
 cv2.drawContours(mask, blocks, -1, (255,0,0), 3)
 	
-# Save the image
+# Save the image for viewing.
 cv2.imwrite('output.jpg',mask)
