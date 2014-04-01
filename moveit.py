ad2b742bbf2c6d9fa03fc30d818ae94ca0f1235d
Kevin Roberts <robe0815@umn.edu>
2014-04-01 11:43:40 -0700

    added moveit.py start

---
+++ moveit.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+
+import rospy
+from moveit_commander import MoveGroupCommander
+from moveit_commander import RobotCommander
+
+if __name__ == '__main__':
+
+    rospy.init_node('moveit_py', anonymous=True)
+    robot = RobotCommander()
+    print robot.get_group_names()
+    #print "hello"
+    #group = MoveGroupCommander("right_arm")
+    # move to a random target
+    x = robot.right_arm.get_current_joint_values()
+    print x
+    x[1] = x[1] + 0.1
+# launching with move_group.launch instead of demo_baxter.launch should get rid of rviz
+# if this has no end effector uses end effector from get_end_effector_link
+# http://moveit.ros.org/doxygen/classmoveit_1_1planning__interface_1_1MoveGroup.html#a4e8b5b43b1df5e8f3e8c95c25387f45f
+    robot.right_arm.set_joint_value_target(x,"right_hand")
+    robot.right_arm.go()
+    rospy.sleep(1)
+    rospy.spin()
+
