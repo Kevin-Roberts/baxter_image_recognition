768108b7afc7500b7750467785a0bbeab20576e8
SamKoehnen <koehn087@umn.edu>
2014-04-15 17:45:32 -0500

    Added are_blocks_near() to check for close blocks. Still needs Debugging.

--- src/master_controller.py
+++ src/master_controller.py
@@ -5,6 +5,7 @@ import math
 from move_controller import MoveController
 from image_receiver import ImageReceiver
 from image_processor import ImageProcessor
+from block import Block
 
 class MasterController(object):
     
@@ -36,10 +37,10 @@ class MasterController(object):
         self.greenblocklist = self.ip.findBlock("GREEN")
 
     def position_above_pose(self, pose):
-	z_offset = .02
-	pose.position.z += z_offset
-	self.move.move_to_pose(pose)
-	pose.position.z -= z_offset
+        z_offset = .02
+        pose.position.z += z_offset
+        self.move.move_to_pose(pose)
+        pose.position.z -= z_offset
 
     def align_pose(self, pose, color):
 	self.ip.setImage(self.ir.getImage())
@@ -57,12 +58,68 @@ class MasterController(object):
         #self.rh.pick_at_pose(block.pose)
         #self.rh.drop_at_pose(box.(block.color).pose) #this will probably change
 
-#Might be nice to have a block class (box class too?). Also, might be useful in image_processor.py
-class Block(object):
+    def are_blocks_near(self, block):
+        xthresh = 0
+        ythresh = .064 #~2.5 inches
+        xpose = block.pose.position.x
+        ypose = block.pose.position.y
+        isclose = False
+        
+        print "Current block:________"
+        print block.pose.position
 
-    def __init__(self, color, pos = None):
-        self.color = color
-        self.pose = pose
+        #check for close blue blocks
+        for block2 in self.blueblocklist:
+            print "Blue block at:"
+            print block2.pose.position
+            if (xpose != block2.pose.position.x) and (ypose != block2.pose.position.y):
+                if (math.fabs(xpose - block2.pose.position.x) < xthresh):
+                    print "Block at "
+                    print block.pose
+                    print "is close to green block in the y direction."
+                    isclose = True
+                if (math.fabs(ypose - self.blueblocklist[i].pose.position.y) < ythresh):
+                    print "Block at "
+                    print block.pose
+                    print "is close to green block in the y direction."
+                    isclose = True
+
+        #check for close orange blocks
+        for block2 in self.orangeblocklist:
+            print "Orange block at:"
+            print block2.pose.position
+            if (xpose != block2.pose.position.x) and (ypose != block2.pose.position.y):
+                if (math.fabs(xpose - block2.pose.position.x) < xthresh):
+                    print "Block at "
+                    print block.pose
+                    print "is close to green block in the y direction."
+                    isclose = True
+                if (math.fabs(ypose - block2.pose.position.y) < ythresh):
+                    print "Block at "
+                    print block.pose
+                    print "is close to green block in the y direction."
+                    isclose = True
+
+
+        #check for close blue blocks
+        for i in self.greenblocklist:
+            print "Green block at:"
+            print block2.pose.position
+            if (xpose != block2.pose.position.x) and (ypose != block2.pose.position.y):
+                if (math.fabs(xpose - block2.pose.position.x) < xthresh):
+                    print "Block at "
+                    print block.pose
+                    print "is close to green block in the y direction."
+                    isclose = True
+                if (math.fabs(ypose - block2.pose.position.y) < ythresh):
+                    print "Block at "
+                    print block.pose
+                    print "is close to green block in the y direction."
+                    isclose = True
+
+        return isclose
+
+        
         
 
 def main():
@@ -71,14 +128,19 @@ def main():
     #mc.get_image()
     while True:
         mc.find_blocks()
+        print "pose:"
         print mc.orangeblocklist[0].pose
         #mc.rh.move_to_pose(mc.blueblocklist[0].pose)
-	mc.position_above_pose(mc.orangeblocklist[0].pose)
-	mc.align_pose(mc.orangeblocklist[0].pose, "ORANGE")
-        mc.move.pick_at_pose(mc.orangeblocklist[0].pose)
-        mc.move.drop_at_pose(mc.move.home_pose)
+ #	mc.position_above_pose(mc.orangeblocklist[0].pose)
+ #	mc.align_pose(mc.orangeblocklist[0].pose, "ORANGE")
+ #       mc.move.pick_at_pose(mc.orangeblocklist[0].pose)
+ #       mc.move.drop_at_pose(mc.move.home_pose)
         #mc.rh.pick_at_pose(mc.rh.home_pose)
-
+        print "position:"
+        print mc.orangeblocklist[0].pose.position.x
+        if mc.are_blocks_near(mc.orangeblocklist[0]):
+            print "block near"
+        break
 
     #mc.rh.pick_at_pose(mc.rh.home_pose)
 
